58dec49acd1f5d7df341f594ed9bc65d099cfa1d
jac267 <64326015+jac267@users.noreply.github.com>
2023-04-27 19:37:00 -0400

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,3 @@
+//Le fichier app.js contiendra la logique principale de votre application React, y compris la définition de vos composants,
+//la gestion des états, la récupération de données à partir d'une API, etc.
+//Vous pouvez également y définir des routes pour votre application en utilisant une bibliothèque de routage telle que React Router.
---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div class="section" id="confiez-nous-vos-projets">
+      <h1>Confiez-nous vos projets</h1>
+      <p>
+        Nos spécialistes en génie civil et travaux de voirie vous proposent
+        <br></br>
+        des solutions résidentielles commericales et institutionnelles adaptées
+        à vos besoins
+      </p>
+      <button>Contactez-nous</button>
+    </div>
+  );
+}
