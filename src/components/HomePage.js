111fdfcc7f4f938cb5f08f942bade52b242afdfc
jac267 <jackpotchap@gmail.com>
2023-04-27 21:05:05 -0400

    contactez nous

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,18 @@
 function Home() {
   return (
     <div class="section" id="confiez-nous-vos-projets">
-      <h1>Confiez-nous vos projets</h1>
-      <p>
-        Nos spécialistes en génie civil et travaux de voirie vous proposent
-        <br></br>
-        des solutions résidentielles commericales et institutionnelles adaptées
-        à vos besoins
-      </p>
-      <button>Contactez-nous</button>
+      <div id="filtre-confiez-nous-vos-projets">
+        <div>
+          <h1>Confiez-nous vos projets</h1>
+          <p>
+            Nos spécialistes en génie civil et travaux de voirie vous proposent
+            <br></br>
+            des solutions résidentielles commericales et institutionnelles
+            adaptées à vos besoins
+          </p>
+          <button>Contactez-nous</button>
+        </div>
+      </div>
     </div>
   );
 }
