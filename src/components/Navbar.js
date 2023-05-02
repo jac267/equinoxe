7499bbd86906975faf552b23e4c68a7ffdef0dd3
jac267 <jackpotchap@gmail.com>
2023-05-02 14:59:37 -0400

    debut nav bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+function NavBar() {
+  return (
+    <div id="nav-bar">
+      <img src="../../public/images/galerie3.jpg"></img>
+      <div>
+        <div id="logo">
+          <img src="../../public/images/cropped-logo-equinox-9.png"></img>
+        </div>
+        <div id="telephone">819-669-7500</div>
+        <div id="email">info@equinoxejmp.com</div>
+        <button id="acceuil">ACCEUIL</button>
+        <button id="notre-equipe">NOTRE ÉQUIPE</button>
+        <button id="services">SERVICES</button>
+        <button id="realisation">RÉALISATION</button>
+      </div>
+    </div>
+  );
+}
