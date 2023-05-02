a0ddcdef404552c1ed499e374010e14aca4e0645
jac267 <jackpotchap@gmail.com>
2023-05-01 20:05:50 -0400

    pratnaire

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,6 +4,7 @@ function Home() {
       <HomeConfiezNousVosProjets />
       <HomeVotreEntrepreneurGeneral />
       <HomeNosServices1 />
+      <HomeNosServices2 />
     </div>
   );
 }
@@ -120,6 +121,61 @@ function HomeNosServices1() {
               Carrière et sablière pour tous les matériaux granulaires certifiés
             </li>
           </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function HomeNosServices2() {
+  return (
+    <div id="home-big-nos-services2">
+      <div id="home-nos-services2" class="flexRow">
+        <div class="flexColumn" id="home-nos-services2-content">
+          <h2>Votre partenaire de choix</h2>
+          <p>
+            Chez Equinoxe JMP, nous sommes d’avis qu’un entrepreneur général
+            doit être beaucoup plus qu’un simple fournisseur de services. Il
+            doit selon nous constituer un partenaire de premier plan afin
+            d’offrir des solutions personnalisées répondant parfaitement aux
+            besoins du client.
+          </p>
+          <h3>Choisir Equinoxe JMP, c’est opter pour :</h3>
+          <div class="flexRow">
+            <ul>
+              <li>un partenariat d’affaires</li>
+
+              <li>une équipe dynamique et multidisciplinaire</li>
+
+              <li>un contremaître expérimenté sur chaque chantier</li>
+
+              <li>une main-d’œuvre compétente</li>
+
+              <li>une relation solide et chaleureuse</li>
+            </ul>
+            <ul>
+              <li>un suivi serré des budgets et des échéanciers</li>
+
+              <li>une planification efficace et rigoureuse</li>
+
+              <li>une expertise reconnue</li>
+
+              <li>une capacité d’adaptation aux différents défis</li>
+
+              <li>
+                une entreprise à l’affût des dernières tendances de l’industrie
+              </li>
+            </ul>
+          </div>
+          <p class="Smallerpxfont">
+            Contactez-nous sans attendre pour avoir plus d’information sur nos
+            services. De plus, notre expertise et notre vaste réseau de contacts
+            nous permettent de réagir rapidement afin de trouver des solutions
+            économiques et avantageuses lors des défis rencontrés.
+          </p>
+        </div>
+        <div id="home-nos-services2-image1" class="home-nos-services2-images">
+          {" "}
         </div>
       </div>
     </div>
