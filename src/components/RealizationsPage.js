1d424ce84d82579ef74d31fa2a83b5e09f1a8b11
LpCote <loulou4@live.ca>
2023-05-08 23:32:51 -0400

    now or never

--- src/components/RealizationsPage.js
+++ src/components/RealizationsPage.js
@@ -34,62 +34,62 @@ function GallerieExcavation(){
         <div className="sub-container">
           <div className="grid" id="my-gallery1">
               <a
-              href="/equinoxe/public/images/excavation1.JPEG"
+              href="../../../equinoxe/public/images/excavation1.JPEG"
               data-lightbox="excavation"
               data-title="Travaux d'excavation"
               className="img"
               id="img1"
               >
               <img
-                  src="/equinoxe/public/images/excavation1.JPEG"
+                  src="../../../equinoxe/public/images/excavation1.JPEG"
                   alt=""
               />
               </a>
               <a
-              href="/equinoxe/public/images/excavation2.jpg"
+              href="../../../equinoxe/public/images/excavation2.jpg"
               data-lightbox="excavation"
               data-title="Travaux d'excavation"
               className="img"
               id="img2"
               >
               <img
-                  src="/equinoxe/public/images/excavation2.jpg"
+                  src="../../../equinoxe/public/images/excavation2.jpg"
                   alt=""
               />
               </a>
               <a
-              href="/equinoxe/public/images/excavation3.jpg"
+              href="../../../equinoxe/public/images/excavation3.jpg"
               data-lightbox="excavation"
               data-title="Travaux d'excavation"
               className="img"
               id="img3"
               >
               <img
-                  src="/equinoxe/public/images/excavation3.jpg"
+                  src="../../../equinoxe/public/images/excavation3.jpg"
                   alt=""
               />
               </a>
               <a
-              href="/equinoxe/public/images/excavation4.jpg"
+              href="../../../equinoxe/public/images/excavation4.jpg"
               data-lightbox="excavation"
               data-title="Travaux d'excavation"
               className="img"
               id="img4"
               >
               <img
-                  src="/equinoxe/public/images/excavation4.jpg"
+                  src="../../../equinoxe/public/images/excavation4.jpg"
                   alt=""
               />
               </a>
               <a
-              href="/equinoxe/public/images/excavation5.JPEG"
+              href="../../../equinoxe/public/images/excavation5.JPEG"
               data-lightbox="excavation"
               data-title="Travaux d'excavation"
               className="img"
               id="img5"
               >
               <img
-                  src="/equinoxe/public/images/excavation5.JPEG"
+                  src="../../../equinoxe/public/images/excavation5.JPEG"
                   alt=""
               />
               </a>
@@ -115,74 +115,74 @@ function GallerieGenieCivil(){
           </div>
           <div className="grid" id="my-gallery2">
               <a
-              href="/equinoxe/public/images/genie1.jpg"
+              href="../../../equinoxe/public/images/genie1.jpg"
               data-lightbox="genie"
               data-title="Projets de génie civil"
               className="img"
               id="img6"
               >
               <img
-                  src="/equinoxe/public/images/genie1.jpg"
+                  src="../../../equinoxe/public/images/genie1.jpg"
                   alt=""
               />
               </a>
               <a
-              href="/equinoxe/public/images/genie2.jpg"
+              href="../../../equinoxe/public/images/genie2.jpg"
               data-lightbox="genie"
               data-title="Projets de génie civil"
               className="img"
               id="img7"
               >
               <img
-                  src="/equinoxe/public/images/genie2.jpg"
+                  src="../../../equinoxe/public/images/genie2.jpg"
                   alt=""
               />
               </a>
               <a
-              href="/equinoxe/public/images/genie3.jpg"
+              href="../../../equinoxe/public/images/genie3.jpg"
               data-lightbox="genie"
               data-title="Projets de génie civil"
               className="img"
               id="img8"
               >
               <img
-                  src="/equinoxe/public/images/genie3.jpg"
+                  src="../../../equinoxe/public/images/genie3.jpg"
                   alt=""
               />
               </a>
               <a
-              href="/equinoxe/public/images/genie4.jpg"
+              href="../../../equinoxe/public/images/genie4.jpg"
               data-lightbox="genie"
               data-title="Projets de génie civil"
               className="img"
               id="img9"
               >
               <img
-                  src="/equinoxe/public/images/genie4.jpg"
+                  src="../../../equinoxe/public/images/genie4.jpg"
                   alt=""
               />
               </a>
               <a
-              href="/equinoxe/public/images/genie5.png"
+              href="../../../equinoxe/public/images/genie5.png"
               data-lightbox="genie"
               data-title="Projets de génie civil"
               className="img"
               id="img10"
               >
               <img
-                  src="/equinoxe/public/images/genie5.png"
+                  src="../../../equinoxe/public/images/genie5.png"
                   alt=""
               />
               </a>
               <a
-              href="/equinoxe/public/images/genie6.jpg"
+              href="../../../equinoxe/public/images/genie6.jpg"
               data-lightbox="genie"
               data-title="Projets de génie civil"
               className="img"
               id="img11"
               >
               <img
-                  src="/equinoxe/public/images/genie6.jpg"
+                  src="../../../equinoxe/public/images/genie6.jpg"
                   alt=""
               />
               </a>
